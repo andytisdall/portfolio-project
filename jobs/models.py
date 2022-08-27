e2ea144f399227ad4a9667b3e0e8e660f2fe5c75
thatsongsucks <andrew.tisdall@gmail.com>
2022-08-27 01:40:17 -0700

    change date field in shows to an actual date field

--- jobs/models.py
+++ jobs/models.py
@@ -1,19 +1,22 @@
 from django.db import models
 
+
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
+
 
 class Show(models.Model):
     venue = models.CharField(max_length=200)
     venue_url = models.URLField()
     bands = models.CharField(max_length=200)
-    date = models.CharField(max_length=200)
+    date = models.DateField(max_length=200)
     time = models.TimeField()
     flyer = models.ImageField(upload_to='images/')
 
     def __str__(self):
         return self.date
+
 
 class MailingListPerson(models.Model):
     name = models.CharField(max_length=50)
@@ -21,6 +24,7 @@ class MailingListPerson(models.Model):
 
     def __str__(self):
         return self.name
+
 
 class Demo(models.Model):
     name = models.CharField(max_length=100)
