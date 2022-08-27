e667067f3d54531e1fd07b2c3f6b42708f186ee1
thatsongsucks <andrew.tisdall@gmail.com>
2022-08-27 02:20:35 -0700

    adjust spinner behavior when audio is being loaded

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Job, Show, MailingListPerson
+from .models import Job, MailingListPerson
 
 admin.site.register(Job)
-admin.site.register(Show)
+# admin.site.register(Show)
 admin.site.register(MailingListPerson)
