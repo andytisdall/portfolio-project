cad54d064edd0115287774de9155517ec91d7720
thatsongsucks <andrew.tisdall@gmail.com>
2022-08-27 18:59:30 -0700

    one more time

--- lyrics/admin.py
+++ lyrics/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Lyrics, Album, Show
+from .models import Lyrics, Album
 
 admin.site.register(Lyrics)
 admin.site.register(Album)
-admin.site.register(Show)
+# admin.site.register(Show)
